6944b7c69de6e7cc219ede72365c91d448193475
Kwizera Ishimwe Leontine <ika22001@student.mdu.se>
2024-07-03 09:37:34 +0200

    new changes

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello world! here is the app.py file")
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+
+YouTube_url = 'https://www.youtube.com/watch?v=5BKLjO_WvRk'
+
+video_id = YouTube(YouTube_url).video_id
+
+transcript = YouTubeTranscriptApi.get_transcript(video_id)
+
+text_transcript = ''
+
+for segment in transcript:
+    text_transcript += segment['text'] + ' '
+
+print(text_transcript)
